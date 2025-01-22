810c768a8be55ed8440d976479023d2c9b114430
mariorojas2013 <mario.rivra@hotmail.com>
2025-01-22 13:07:12 -0500

    Add files via upload

--- functions/api.js
+++ functions/api.js
@@ -14,45 +14,9 @@ ctx.fillStyle = "white";
 ctx.fillText('escribiendo algo', 50, 100);
 const buffer = canvas.toBuffer('image/png');
 res.writeHead(200, { 'Content-Type': 'image/png' });
-res.end(buffer);
-  // res.send('App is running..');
+ 
 });
-
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
+ 
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
